a7f3ac34363d9ca84c727a6327f0e204beffeb69
SumeelaMadusankha <hasumeela@gmail.com>
2021-11-22 00:13:22 +0530

    changess adde for register user

--- APP/models/User_Model.php
+++ APP/models/User_Model.php
@@ -19,9 +19,24 @@ class User_Model extends Model{
         $jobType=$dataArray["jobType"];
         $city=$dataArray["city"];
        
-       $quary1="INSERT INTO user (nic, firstName, LastName, dob, address, district,gender,bloodGroup,email,mobileNo,jobType,city) VALUES ('$nic','$firstName',$lastName','$dob','$address','$district',$gender,'$bloodGroup',' $email','$mobileNo','$jobType','$city')";
-       $results= $this->db->runQuery($quary1);
+       $quary1="INSERT INTO user (nic, firstName, LastName, dob, address, district,gender,bloodGroup,email,mobileNo,jobType,city) VALUES (:nic,:firstName,:lastName,:dob,:address,:district,:gender,:bloodGroup, :email,:mobileNo,:jobType,:city)";
+      
+       $arrayInject=[
+           ':nic'=>$nic,
+            ':firstName'=>$firstName,
+            ':lastName'=>$lastName, 
+            ':dob'=>$dob, 
+            ':address'=>$address, 
+            ':district'=>$district, 
+            ':gender'=>$gender, 
+            ':bloodGroup'=>$bloodGroup, 
+            ':email'=>$email, 
+            ':mobileNo'=>$mobileNo, 
+            ':jobType'=>$jobType, 
+            ':city'=>$city];
+       $results= $this->db->runQuery($quary1,$arrayInject);
        return $results;
     }
 }
 ?>
+
