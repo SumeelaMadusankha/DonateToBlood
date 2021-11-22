a5599442b9ab6b47f111f92594462844776539cb
Dinuka-Thathsara <dinukathathsara@gmail.com>
2021-11-22 16:57:55 +0530

    localhost address

--- APP/views/bbc_header.php
+++ APP/views/bbc_header.php
@@ -29,7 +29,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="BB_Coordinater" style="background-color: crimson;">Blood Bank</a>
+                <a class="navbar-brand" href="http://localhost/DonateToBlood/BB_Coordinater" style="background-color: crimson;">Blood Bank</a>
             </div>
 
             <ul class="nav navbar-top-links navbar-right">
