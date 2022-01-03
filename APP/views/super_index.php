384d75623c507ba4f8abbd80a4451248453be5fe
SumeelaMadusankha <hasumeela@gmail.com>
2022-01-03 21:04:22 +0530

    test876876

---
+++ APP/views/super_index.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+    </script>
+    <script>
+        $(function(){
+            $('#super_header').load('super_header.html')
+        })
+    </script>
+</head>
+<body>   
+    <div id="super_header"></div>
+    <div class="main-content">
+        <main>
+            <div class="cards">
+                <div class="card-single">
+                    <div>
+                        <h1>24</h1>
+                        <span>Blood Bank Coordinators</span>
+                    </div>
+                    <div>
+                        <span class="las la-user-circle"></span>
+                    </div>
+                </div>
+                <div class="card-single">
+                    <div>
+                        <h1>24</h1>
+                        <span>Districts</span>
+                    </div>
+                    <div>
+                        <span class="las la-home"></span>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    
+</body>
+</html>
